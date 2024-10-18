ea7d8b046e050d62807560e2a894bc141db01c36
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2024-10-18 17:52:46 +0200

    edit: app.scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AuthenticationComponent } from './core/components/authentication/authentication.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthenticationComponent,
+  },
+];
