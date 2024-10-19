46d0738391ce08d43534285ee39825b3c8b3c23d
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2024-10-19 18:23:05 +0200

    style: left-side header

---
+++ src/app/core/components/main/left-side-component/chat-list/chat-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-list.component.html',
+  styleUrl: './chat-list.component.scss'
+})
+export class ChatListComponent {
+
+}
--- src/app/core/components/main/left-side-component/left-side-component.component.ts
+++ src/app/core/components/main/left-side-component/left-side-component.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ChannelListComponent } from './channel-list/channel-list.component';
+import { ChatListComponent } from './chat-list/chat-list.component';
+import { ChannelChatComponent } from '../main-workspace/channel-chat/channel-chat.component';
 
 @Component({
   selector: 'app-left-side-component',
   standalone: true,
-  imports: [],
+  imports: [ChannelListComponent, ChatListComponent, ChannelChatComponent],
   templateUrl: './left-side-component.component.html',
-  styleUrl: './left-side-component.component.scss'
+  styleUrl: './left-side-component.component.scss',
 })
-export class LeftSideComponentComponent {
-
-}
+export class LeftSideComponentComponent {}
