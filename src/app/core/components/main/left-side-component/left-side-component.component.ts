cdac791a3d1ff31e217e3fe8453a56a2fe2deca9
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2024-10-20 13:49:30 +0200

    style: devspace

--- src/app/core/components/main/left-side-component/left-side-component.component.ts
+++ src/app/core/components/main/left-side-component/left-side-component.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { ChannelListComponent } from './channel-list/channel-list.component';
 import { ChatListComponent } from './chat-list/chat-list.component';
-import { ChannelChatComponent } from '../main-workspace/channel-chat/channel-chat.component';
+import { ChannelListComponent } from './channel-list/channel-list.component';
 
 @Component({
   selector: 'app-left-side-component',
   standalone: true,
-  imports: [ChannelListComponent, ChatListComponent, ChannelChatComponent],
+  imports: [ChannelListComponent, ChatListComponent],
   templateUrl: './left-side-component.component.html',
   styleUrl: './left-side-component.component.scss',
 })
