063a6bd285156edcbbb6a575b20cb67092ae0b93
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2024-10-20 16:57:02 +0200

    style: devspace

--- src/app/core/components/main/left-side-component/channel-list/channel-list.component.ts
+++ src/app/core/components/main/left-side-component/channel-list/channel-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-channel-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './channel-list.component.html',
-  styleUrl: './channel-list.component.scss'
+  styleUrl: './channel-list.component.scss',
 })
 export class ChannelListComponent {
+  channelListOpen: boolean = false;
 
+  toggleChannelList() {
+    this.channelListOpen = !this.channelListOpen;
+  }
 }
--- src/app/core/components/main/left-side-component/chat-list/chat-list.component.ts
+++ src/app/core/components/main/left-side-component/chat-list/chat-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-list.component.html',
-  styleUrl: './chat-list.component.scss'
+  styleUrl: './chat-list.component.scss',
 })
 export class ChatListComponent {
+  chatListOpen: boolean = false;
 
+  toggleChatlList() {
+    this.chatListOpen = !this.chatListOpen;
+  }
 }
