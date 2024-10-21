1cd836ec802c445ad72120bd824322f333a5a243
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2024-10-21 21:04:09 +0200

    style: devspace show hide button

--- src/app/core/components/main/main.component.ts
+++ src/app/core/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MainWorkspaceComponent } from './main-workspace/main-workspace.component';
 import { LeftSideComponentComponent } from './left-side-component/left-side-component.component';
 import { RightSideContainerComponent } from './right-side-container/right-side-container.component';
@@ -12,8 +13,24 @@ import { HeaderComponent } from './header/header.component';
     LeftSideComponentComponent,
     RightSideContainerComponent,
     HeaderComponent,
+    CommonModule,
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent {
+  workSpaceOpen: boolean = false;
+  workspaceButtonText: string = 'Workspace-Menu schließen';
+
+  toggleWorkspace() {
+    this.workSpaceOpen = !this.workSpaceOpen;
+  }
+
+  changeText() {
+    if (this.workSpaceOpen) {
+      this.workspaceButtonText = 'Workspace-Menu öffnen';
+    } else {
+      this.workspaceButtonText = 'Workspace-Menu schließen';
+    }
+  }
+}
