06e54148bad175ebafdeff9b03dc66bd173bc99a
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-10-22 16:17:03 +0200

    feat, style: ChooseAvatarComponent

--- src/app/core/components/choose-avatar/choose-avatar.component.ts
+++ src/app/core/components/choose-avatar/choose-avatar.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { AuthUIService } from '../../shared/services/authUI-services/authUI.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-choose-avatar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './choose-avatar.component.html',
-  styleUrl: './choose-avatar.component.scss'
+  styleUrl: './choose-avatar.component.scss',
 })
 export class ChooseAvatarComponent {
+  constructor(public authUIService: AuthUIService) {}
 
+  photos: string[] = [
+    'assets/img/profile-img/Elias-Neumann.svg',
+    'assets/img/profile-img/Elise-Roth.svg',
+    'assets/img/profile-img/Frederik-Beck.svg',
+    'assets/img/profile-img/Noah-Braun.svg',
+    'assets/img/profile-img/Sofia-Müller.svg',
+    'assets/img/profile-img/Steffen-Hoffmann.svg',
+  ];
 }
