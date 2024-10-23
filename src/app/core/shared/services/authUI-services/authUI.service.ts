bc5e152f526dfcd52c7f3e408b25141848c8ec9a
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-10-23 15:48:30 +0200

    refactor:  authentication

--- src/app/core/shared/services/authUI-services/authUI.service.ts
+++ src/app/core/shared/services/authUI-services/authUI.service.ts
@@ -13,18 +13,73 @@ export class AuthUIService {
 
   toggleSignup() {
     this.showSignup = !this.showSignup;
+    if (this.showSignup) {
+      this.showAvatarSelection = false;
+      this.showResetPassword = false;
+    }
   }
 
   toggleAvatarSelection() {
     this.showAvatarSelection = true;
+    this.showSignup = false;
   }
 
   toggleResetPassword() {
     this.showResetPassword = true;
+    this.showSignup = false;
+    this.showAvatarSelection = false;
   }
 
   toggleCheckbox() {
     this.isChecked = !this.isChecked;
+  }
+
+  getAuthWrapperHeight() {
+    if (
+      this.showResetPassword &&
+      !this.showSignup &&
+      !this.showAvatarSelection
+    ) {
+      return '469px';
+    } else if (this.showSignup || this.showAvatarSelection) {
+      return '669px';
+    } else {
+      return '769px';
+    }
+  }
+
+  getAuthWrapperWidth() {
+    if (
+      this.showResetPassword &&
+      !this.showSignup &&
+      !this.showAvatarSelection
+    ) {
+      return '698px';
+    } else if (this.showSignup || this.showAvatarSelection) {
+      return '606px';
+    } else {
+      return '606px';
+    }
+  }
+
+  shouldShowLogin(): boolean {
+    return (
+      !this.showSignup && !this.showAvatarSelection && !this.showResetPassword
+    );
+  }
+
+  shouldShowSignup(): boolean {
+    return (
+      this.showSignup && !this.showAvatarSelection && !this.showResetPassword
+    );
+  }
+
+  shouldShowAvatarSelection(): boolean {
+    return this.showAvatarSelection;
+  }
+
+  shouldShowResetPassword(): boolean {
+    return this.showResetPassword && !this.showAvatarSelection;
   }
 
   focused: { [key: string]: boolean } = {
