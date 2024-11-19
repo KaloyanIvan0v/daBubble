f918265b8e5ec27988a64380de954c2c35587388
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2024-11-19 23:46:09 +0100

    feat: add reactions menu

---
+++ src/app/core/shared/components/message/reactions-menu/reactions-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactions-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './reactions-menu.component.html',
+  styleUrl: './reactions-menu.component.scss'
+})
+export class ReactionsMenuComponent {
+
+}
