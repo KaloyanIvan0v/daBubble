f525db2404b54f5f9ddf6c4e948950d6c59faaf4
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2024-11-22 22:27:38 +0100

    style: messages

--- src/app/core/shared/components/message/message.component.ts
+++ src/app/core/shared/components/message/message.component.ts
@@ -1,16 +1,15 @@
 import { FirebaseServicesService } from './../../services/firebase/firebase.service';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Message } from 'src/app/core/shared/models/message.class';
 import { User } from '../../models/user.class';
-import { Observable, map, firstValueFrom } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { FirebaseTimePipe } from 'src/app/shared/pipes/firebase-time.pipe';
 import { WorkspaceService } from '../../services/workspace-service/workspace.service';
 import { ReactionsMenuComponent } from './reactions-menu/reactions-menu.component';
 import { EmojiPickerComponent } from '../emoji-picker/emoji-picker.component';
 import { EmojiPickerService } from '../../services/emoji-picker/emoji-picker.service';
-import { Reaction } from 'src/app/core/shared/models/reaction.class';
-import { collection } from '@angular/fire/firestore';
+import { AuthService } from '../../services/auth-services/auth.service';
 
 @Component({
   selector: 'app-message',
@@ -28,18 +27,28 @@ export class MessageComponent {
   @Input() message!: Message;
   author$: Observable<User> = new Observable();
   showEmojiPicker = false;
+  loggedInUserId: string | null = '';
+  containerRef!: ElementRef;
+  @ViewChild('messageElement', { static: true }) messageElement!: ElementRef;
+  popupDirection: 'up' | 'down' = 'down';
 
   constructor(
     private firebaseService: FirebaseServicesService,
     private workspaceService: WorkspaceService,
-    private emojiPickerService: EmojiPickerService
-  ) {}
+    private emojiPickerService: EmojiPickerService,
+    private authService: AuthService
+  ) {
+    this.authService.getCurrentUserUID().then((uid) => {
+      this.loggedInUserId = uid;
+    });
+  }
 
   onEmojiSelected(emoji: string) {
     this.emojiPickerService.addReaction(emoji, this.message);
   }
 
   toggleEmojiPicker() {
+    //this.calculatePopupDirection();
     this.showEmojiPicker = !this.showEmojiPicker;
   }
 
@@ -60,5 +69,20 @@ export class MessageComponent {
     return this.firebaseService
       .getUser(uid)
       .pipe(map((user: any) => user?.name || 'Unbekannt'));
+  }
+
+  calculatePopupDirection() {
+    const messageRect =
+      this.messageElement.nativeElement.getBoundingClientRect();
+    const containerRect =
+      this.containerRef.nativeElement.getBoundingClientRect();
+
+    const distanceToBottom = containerRect.bottom - messageRect.bottom;
+
+    if (distanceToBottom <= 200) {
+      this.popupDirection = 'up';
+    } else {
+      this.popupDirection = 'down';
+    }
   }
 }
