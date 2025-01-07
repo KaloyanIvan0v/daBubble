1e7ef3690d825da6ba62420a06aa049f75d1718d
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2025-01-07 22:24:08 +0100

    refactor: chat.ts

--- src/app/core/shared/components/channels-list/channels-list.component.ts
+++ src/app/core/shared/components/channels-list/channels-list.component.ts
@@ -1,9 +1,8 @@
-import { AuthService } from 'src/app/core/shared/services/auth-services/auth.service';
-import { FirebaseServicesService } from 'src/app/core/shared/services/firebase/firebase.service';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from 'src/app/core/shared/services/auth-services/auth.service';
+import { FirebaseServicesService } from 'src/app/core/shared/services/firebase/firebase.service';
 import { WorkspaceService } from 'src/app/core/shared/services/workspace-service/workspace.service';
-import { Subscription } from 'rxjs';
 import { Channel } from '../../models/channel.class';
 
 @Component({
@@ -11,27 +10,36 @@ import { Channel } from '../../models/channel.class';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './channels-list.component.html',
-  styleUrl: './channels-list.component.scss',
+  styleUrls: ['./channels-list.component.scss'],
 })
 export class ChannelsListComponent {
-  currentUserUid: string | null = null;
+  /**
+   * List of channels passed into this component.
+   */
   @Input() channelsInput: Channel[] = [];
-  @Output() selectedChannel = new EventEmitter<string>();
-  private subscriptions: Subscription[] = [];
 
+  /**
+   * Emits the ID of the selected channel.
+   */
+  @Output() selectedChannel = new EventEmitter<string>();
+
+  /**
+   * Creates an instance of ChannelsListComponent.
+   * @param firebaseService - Firebase service for data operations
+   * @param workspaceService - Workspace service for managing workspace state
+   * @param authService - Authentication service
+   */
   constructor(
     public firebaseService: FirebaseServicesService,
     public workspaceService: WorkspaceService,
-    public AuthService: AuthService
+    public authService: AuthService
   ) {}
 
-  returnChannelId(channelId: string) {
-    console.log('Channel ID:', channelId);
-
+  /**
+   * Emits the ID of the clicked channel to notify parent components.
+   * @param channelId - The ID of the clicked channel
+   */
+  returnChannelId(channelId: string): void {
     this.selectedChannel.emit(channelId);
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 }
--- src/app/core/shared/components/chat/chat.component.ts
+++ src/app/core/shared/components/chat/chat.component.ts
@@ -7,7 +7,7 @@ import {
   ElementRef,
   SimpleChanges,
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Timestamp } from '@angular/fire/firestore';
 import { FirebaseDatePipe } from 'src/app/shared/pipes/firebase-date.pipe';
@@ -30,32 +30,75 @@ export class ChatComponent {
   firstScrollDone: boolean = false;
 
   private lastMessageLength: number = 0;
+
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {
+  /**
+   * Lifecycle hook that initializes the component.
+   * Subscribes to query parameters to handle message scrolling.
+   */
+  ngOnInit(): void {
     this.firstScrollDone = false;
-    this.route.queryParamMap.subscribe((params) => {
-      this.messageToScrollTo = params.get('messageId') || null;
-      if (this.messageToScrollTo) {
-        this.scrollToMessageIfNeeded(true);
-        this.firstScrollDone = true;
-      } else {
-        if (!this.messages.length) return;
-        this.setMessageToLastMessageToScroll();
-        this.scrollToMessageIfNeeded(false);
-      }
+    this.subscribeToQueryParams();
+  }
+
+  /**
+   * Subscribes to the route's query parameters and handles scrolling logic.
+   */
+  private subscribeToQueryParams(): void {
+    this.route.queryParamMap.subscribe((params: ParamMap) => {
+      this.handleQueryParams(params);
     });
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['messages']) {
-      setTimeout(() => {
-        this.checkForNewMessages();
-      }, 200);
+  /**
+   * Handles the query parameters to determine which message to scroll to.
+   * @param params - The current route's query parameters
+   */
+  private handleQueryParams(params: ParamMap): void {
+    this.messageToScrollTo = params.get('messageId') || null;
+    if (this.messageToScrollTo) {
+      this.scrollToMessageIfNeeded(true);
+      this.firstScrollDone = true;
+    } else {
+      this.handleNoMessageId();
     }
   }
 
-  scrollToMessageIfNeeded(useSmooth: boolean = true) {
+  /**
+   * Handles the scenario when no specific message ID is provided in the query parameters.
+   */
+  private handleNoMessageId(): void {
+    if (!this.messages.length) return;
+    this.setMessageToLastMessageToScroll();
+    this.scrollToMessageIfNeeded(false);
+  }
+
+  /**
+   * Lifecycle hook that detects changes to input properties.
+   * Checks for new messages and scrolls if necessary.
+   * @param changes - An object of current and previous property values
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['messages']) {
+      this.scheduleCheckForNewMessages();
+    }
+  }
+
+  /**
+   * Schedules a check for new messages after a short delay.
+   */
+  private scheduleCheckForNewMessages(): void {
+    setTimeout(() => {
+      this.checkForNewMessages();
+    }, 200);
+  }
+
+  /**
+   * Scrolls to a specific message if needed.
+   * @param useSmooth - Determines if the scrolling should be smooth
+   */
+  scrollToMessageIfNeeded(useSmooth: boolean = true): void {
     if (!this.messageToScrollTo) return;
     setTimeout(() => {
       const elem = document.getElementById('message-' + this.messageToScrollTo);
@@ -68,33 +111,89 @@ export class ChatComponent {
     }, 0);
   }
 
+  /**
+   * Checks for new messages and scrolls to the bottom if new messages are detected.
+   */
   private checkForNewMessages(): void {
-    if (this.lastMessageLength !== this.messages.length) {
+    if (this.hasNewMessages()) {
       this.scrollToBottom(true);
-      this.lastMessageLength = this.messages.length;
+      this.updateLastMessageLength();
     }
   }
 
-  setMessageToLastMessageToScroll(): void {
-    this.messageToScrollTo = this.messages[this.messages.length - 1].id;
+  /**
+   * Determines if there are new messages compared to the last known count.
+   * @returns True if new messages are present, otherwise false
+   */
+  private hasNewMessages(): boolean {
+    return this.lastMessageLength !== this.messages.length;
   }
 
+  /**
+   * Updates the last message length to the current messages array length.
+   */
+  private updateLastMessageLength(): void {
+    this.lastMessageLength = this.messages.length;
+  }
+
+  /**
+   * Sets the `messageToScrollTo` property to the ID of the last message in the list.
+   */
+  private setMessageToLastMessageToScroll(): void {
+    this.messageToScrollTo = this.getLastMessageId();
+  }
+
+  /**
+   * Retrieves the ID of the last message in the messages array.
+   * @returns The ID of the last message
+   */
+  private getLastMessageId(): string {
+    return this.messages[this.messages.length - 1].id;
+  }
+
+  /**
+   * Converts a Timestamp, Date, or number to a UNIX timestamp in milliseconds.
+   * @param time - The time value to convert
+   * @returns The UNIX timestamp in milliseconds or undefined if invalid
+   */
   getTimestamp(time: Timestamp | Date | number): number | undefined {
-    if (time instanceof Timestamp) {
-      time = time.toDate();
-    } else if (typeof time === 'number') {
-      time = new Date(time);
-    }
-    return time instanceof Date ? time.getTime() : undefined;
+    const date = this.convertToDate(time);
+    return date ? date.getTime() : undefined;
   }
 
+  /**
+   * Converts various time formats to a Date object.
+   * @param time - The time value to convert
+   * @returns A Date object or undefined if conversion fails
+   */
+  private convertToDate(time: Timestamp | Date | number): Date | undefined {
+    if (time instanceof Timestamp) {
+      return time.toDate();
+    } else if (typeof time === 'number') {
+      return new Date(time);
+    } else if (time instanceof Date) {
+      return time;
+    }
+    return undefined;
+  }
+
+  /**
+   * Determines if the current message is on a new day compared to the previous message.
+   * @param prevTimestamp - The timestamp of the previous message
+   * @param currentTimestamp - The timestamp of the current message
+   * @returns True if it's a new day, otherwise false
+   */
   isNewDay(prevTimestamp?: number, currentTimestamp?: number): boolean {
     if (!prevTimestamp || !currentTimestamp) return false;
-    const prevDate = new Date(prevTimestamp);
-    const currentDate = new Date(currentTimestamp);
-    return !this.isSameDay(prevDate, currentDate);
+    return !this.isSameDay(new Date(prevTimestamp), new Date(currentTimestamp));
   }
 
+  /**
+   * Checks if two dates fall on the same calendar day.
+   * @param date1 - The first date to compare
+   * @param date2 - The second date to compare
+   * @returns True if both dates are on the same day, otherwise false
+   */
   private isSameDay(date1: Date, date2: Date): boolean {
     return (
       date1.getFullYear() === date2.getFullYear() &&
@@ -103,10 +202,18 @@ export class ChatComponent {
     );
   }
 
-  setEditMessage($event: Message) {
-    this.messageToEdit.emit($event);
+  /**
+   * Emits an event to notify parent components that a message is to be edited.
+   * @param message - The message object to edit
+   */
+  setEditMessage(message: Message): void {
+    this.messageToEdit.emit(message);
   }
 
+  /**
+   * Scrolls the message container to the bottom.
+   * @param smooth - Determines if the scrolling should be smooth
+   */
   private scrollToBottom(smooth: boolean = true): void {
     if (!this.messageContainer) return;
     const container = this.messageContainer.nativeElement as HTMLElement;
