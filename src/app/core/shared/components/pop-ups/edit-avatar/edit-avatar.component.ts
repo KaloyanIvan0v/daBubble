460150c005a9fe856a81b00cc0942a5cdf4161ff
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2025-01-07 23:06:06 +0100

    refactor: edit-channel

--- src/app/core/shared/components/pop-ups/edit-avatar/edit-avatar.component.ts
+++ src/app/core/shared/components/pop-ups/edit-avatar/edit-avatar.component.ts
@@ -4,16 +4,24 @@ import { WorkspaceService } from '../../../services/workspace-service/workspace.
 import { UploadCareService } from '../../../services/uploadcare-service/uploadcare.service';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../services/auth-services/auth.service';
+
 @Component({
   selector: 'app-edit-avatar',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './edit-avatar.component.html',
-  styleUrl: './edit-avatar.component.scss',
+  styleUrls: ['./edit-avatar.component.scss'],
 })
 export class EditAvatarComponent {
   userData$: Observable<any>;
 
+  /**
+   * Constructs the EditAvatarComponent with necessary service dependencies.
+   * Initializes the observable for user data.
+   * @param workspaceService Service for managing workspace-related data.
+   * @param uploadCareService Service for handling avatar uploads.
+   * @param authService Service for authentication and user information.
+   */
   constructor(
     public workspaceService: WorkspaceService,
     public uploadCareService: UploadCareService,
@@ -22,23 +30,29 @@ export class EditAvatarComponent {
     this.userData$ = this.workspaceService.loggedInUserData;
   }
 
+  /**
+   * Handles the event when a new photo is selected for the avatar.
+   * Closes the avatar editing popup and triggers the avatar upload process.
+   * @param event The file input event containing the selected file.
+   */
   updatePhoto(event: any) {
     this.workspaceService.editAvatarPopUp.set(false);
-    // this.uploadCareService.onFileSelected(event);
     this.uploadCareService.saveAvatar();
   }
 
-  resetCurrentUserAvatar() {
-    // this.workspaceService.editAvatarPopUp.set(false);
-    // this.uploadCareService.deleteFromUploadcare(
-    //   this.uploadCareService.uploadedFileUuid ?? ''
-    // );
-  }
-
+  /**
+   * Closes the avatar editing popup by updating the workspace service state.
+   * This method is typically called when the user decides to cancel editing the avatar.
+   */
   closePopUp() {
     this.workspaceService.editAvatarPopUp.set(false);
   }
 
+  /**
+   * Getter to determine the visibility of the edit avatar popup.
+   * Retrieves the current state from the workspace service.
+   * @returns A boolean indicating whether the popup is visible.
+   */
   get popUpVisible() {
     return this.workspaceService.editAvatarPopUp();
   }
--- src/app/core/shared/components/pop-ups/edit-channel/edit-channel.component.ts
+++ src/app/core/shared/components/pop-ups/edit-channel/edit-channel.component.ts
@@ -1,12 +1,12 @@
+import { AuthService } from 'src/app/core/shared/services/auth-services/auth.service';
+import { FirebaseServicesService } from 'src/app/core/shared/services/firebase/firebase.service';
 import { Component, effect, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { WorkspaceService } from '../../../services/workspace-service/workspace.service';
-import { FirebaseServicesService } from '../../../services/firebase/firebase.service';
 import { Channel } from 'src/app/core/shared/models/channel.class';
 import { Observable, Subject } from 'rxjs';
 import { User } from 'src/app/core/shared/models/user.class';
-import { AuthService } from 'src/app/core/shared/services/auth-services/auth.service';
 import { Router } from '@angular/router';
 import { takeUntil, first } from 'rxjs/operators';
 import { UserListComponent } from '../../user-list/user-list.component';
@@ -28,6 +28,14 @@ export class EditChannelComponent implements OnDestroy {
   editNameActive: boolean = false;
   editDescriptionActive: boolean = false;
 
+  /**
+   * Constructs the EditChannelComponent with necessary service dependencies.
+   * Initializes the channel data.
+   * @param workspaceService Service for managing workspace-related data.
+   * @param firebaseService Service for interacting with Firebase.
+   * @param authService Service for authentication and user information.
+   * @param router Router service for navigation.
+   */
   constructor(
     public workspaceService: WorkspaceService,
     public firebaseService: FirebaseServicesService,
@@ -37,6 +45,10 @@ export class EditChannelComponent implements OnDestroy {
     this.initializeChannelData();
   }
 
+  /**
+   * Initializes the channel data by setting up reactive effects and subscriptions.
+   * This method sets up an effect to monitor changes in the active channel and subscribes to channel data updates.
+   */
   private initializeChannelData() {
     effect(() => {
       this.currentChannelId = this.workspaceService.currentActiveUnitId();
@@ -48,6 +60,10 @@ export class EditChannelComponent implements OnDestroy {
     });
   }
 
+  /**
+   * Subscribes to the channel data observable to receive updates about the current channel.
+   * It ensures that the subscription is terminated when the component is destroyed.
+   */
   private subscribeToChannelData() {
     this.channelData$
       .pipe(
@@ -59,6 +75,10 @@ export class EditChannelComponent implements OnDestroy {
       });
   }
 
+  /**
+   * Sets the creator of the channel by fetching the creator's user data from Firebase.
+   * @param creatorUid The UID of the channel creator.
+   */
   async setChannelCreator(creatorUid: string) {
     const currentUserUid = await this.authService.getCurrentUserUID();
     if (currentUserUid) {
@@ -66,29 +86,53 @@ export class EditChannelComponent implements OnDestroy {
     }
   }
 
+  /**
+   * Sets the input values for the channel data by subscribing to the channel data observable.
+   * This method updates the local `channelData` property with the latest channel information.
+   */
   setInputValues() {
     this.channelData$.pipe(takeUntil(this.destroy$)).subscribe((data) => {
       this.channelData = data;
     });
   }
 
+  /**
+   * Lifecycle hook that is called when the component is destroyed.
+   * Cleans up all active subscriptions to prevent memory leaks.
+   */
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
+  /**
+   * Toggles the edit mode for the channel name.
+   * If the edit mode is active, it will be deactivated, and vice versa.
+   */
   toggleEditName() {
     this.editNameActive = !this.editNameActive;
   }
 
+  /**
+   * Toggles the edit mode for the channel description.
+   * If the edit mode is active, it will be deactivated, and vice versa.
+   */
   toggleEditDescription() {
     this.editDescriptionActive = !this.editDescriptionActive;
   }
 
+  /**
+   * Closes the edit channel popup by updating the workspace service state.
+   * This method is typically called when the user decides to cancel editing the channel.
+   */
   closeEditChannelPopUp() {
     this.workspaceService.editChannelPopUp.set(false);
   }
 
+  /**
+   * Saves the changes made to the channel's data.
+   * This method updates the channel document in Firebase with the current `channelData`.
+   */
   saveChanges() {
     this.firebaseService.updateDoc<Channel>(
       'channels',
@@ -97,6 +141,14 @@ export class EditChannelComponent implements OnDestroy {
     );
   }
 
+  /**
+   * Allows the current user to leave the channel.
+   * This method performs the following steps:
+   * 1. Removes the current user's UID from the channel's user list.
+   * 2. Saves the updated channel data to Firebase.
+   * 3. Closes the edit channel popup.
+   * 4. Navigates the user to the "new-chat" route.
+   */
   async leaveChannel() {
     await this.removeCurrentUserFromChannel();
     this.saveChanges();
@@ -104,6 +156,10 @@ export class EditChannelComponent implements OnDestroy {
     this.navigateToNewChat();
   }
 
+  /**
+   * Removes the current user's UID from the channel's user list.
+   * This method ensures that the channel's user list is updated to exclude the current user.
+   */
   private async removeCurrentUserFromChannel() {
     const currentLoggedInUserUid = await this.authService.getCurrentUserUID();
     this.channelData.uid = this.channelData.uid.filter(
@@ -111,23 +167,46 @@ export class EditChannelComponent implements OnDestroy {
     );
   }
 
+  /**
+   * Navigates the user to the "new-chat" route.
+   * This is typically called after the user leaves a channel to redirect them to a different view.
+   */
   navigateToNewChat() {
     this.router.navigate(['dashboard', 'new-chat']);
   }
 
+  /**
+   * Getter to determine the visibility of the edit channel popup.
+   * Retrieves the current state from the workspace service.
+   * @returns A boolean indicating whether the popup is visible.
+   */
   get popUpVisible() {
     return this.workspaceService.editChannelPopUp();
   }
 
+  /**
+   * Handles the event when a user is selected from the user list.
+   * Opens the profile view for the selected user.
+   * @param $event The user that was selected.
+   */
   onUserSelected($event: User) {
     this.openProfileView($event.uid);
   }
 
+  /**
+   * Opens the profile view for a specific user.
+   * Sets the active user ID in the workspace service and triggers the profile view popup.
+   * @param uid The UID of the user whose profile is to be viewed.
+   */
   openProfileView(uid: string) {
     this.workspaceService.currentActiveUserId.set(uid);
     this.workspaceService.profileViewPopUp.set(true);
   }
 
+  /**
+   * Opens the popup to add users to the current channel.
+   * This method sets the state in the workspace service to display the add user popup.
+   */
   openAddUserToChannelPopUp() {
     this.workspaceService.addUserToChannelPopUp.set(true);
   }
