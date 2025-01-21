5f05aff78ddeaf2da87269c5c56f037e0f793865
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2025-01-21 14:07:06 +0100

    refactor: firebaseConfig

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,11 +1,10 @@
 export const environment = {
-  production: false, // or true for production
   firebaseConfig: {
-    apiKey: 'AIzaSyDzk2JOhasnRs_b7V5tJUWH2r4R4rUOR1I',
-    authDomain: 'simple-crm-8d1f0.firebaseapp.com',
-    projectId: 'simple-crm-8d1f0',
-    storageBucket: 'simple-crm-8d1f0.appspot.com',
-    messagingSenderId: '954808940147',
-    appId: '1:954808940147:web:25b707a6d57e966fc61c06',
+    apiKey: 'AIzaSyA7J1XqbwEK9zxN8GHqJOoIhIvmKdd4wJI',
+    authDomain: 'dabubble-71ca3.firebaseapp.com',
+    projectId: 'dabubble-71ca3',
+    storageBucket: 'dabubble-71ca3.firebasestorage.app',
+    messagingSenderId: '835335220659',
+    appId: '1:835335220659:web:3a8affcecf55af6ab92fc6',
   },
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyDOkMzVpOFLTles2HVJ2gvDcAbvS9tzxKw',
-    authDomain: 'dabubble-9a2a0.firebaseapp.com',
-    projectId: 'dabubble-9a2a0',
-    storageBucket: 'dabubble-9a2a0.appspot.com',
-    messagingSenderId: '19398386102',
-    appId: '1:19398386102:web:096244dba9f5964599e117',
-    measurementId: 'G-D30K4TPL9B',
+   firebaseConfig :{
+    apiKey: "AIzaSyA7J1XqbwEK9zxN8GHqJOoIhIvmKdd4wJI",
+    authDomain: "dabubble-71ca3.firebaseapp.com",
+    projectId: "dabubble-71ca3",
+    storageBucket: "dabubble-71ca3.firebasestorage.app",
+    messagingSenderId: "835335220659",
+    appId: "1:835335220659:web:3a8affcecf55af6ab92fc6"
   },
 };
